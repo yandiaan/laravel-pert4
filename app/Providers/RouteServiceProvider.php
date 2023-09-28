e723aacdd0f060e40310a81018ddfcd7678e1004
yandiaan <diansetiawan2121@gmail.com>
2023-09-28 07:15:29 +0700

    step 10: RouteServiceProvider

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,6 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
+    protected $namespace = 'App\\Http\\Controllers';
     public const HOME = '/home';
 
     /**
@@ -34,6 +35,7 @@ class RouteServiceProvider extends ServiceProvider
                 ->group(base_path('routes/api.php'));
 
             Route::middleware('web')
+                ->namespace($this->namespace)
                 ->group(base_path('routes/web.php'));
         });
     }
